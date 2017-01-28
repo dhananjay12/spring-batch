fc4279adc5f8aac3802d5d8a05bc31df15e75531
Dhananjay Singh <dhananjay1204@gmail.com>
2017-01-28 17:52:06 +0530

    spring batch basics

---
+++ batch-basics/src/main/java/com/mynotes/spring/batch/BatchBasicsApplication.java
@@ -0,0 +1,14 @@
+package com.mynotes.spring.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class BatchBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchBasicsApplication.class, args);
+	}
+}
